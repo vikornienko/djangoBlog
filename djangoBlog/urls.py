ca343c80af7b894d01f77746d5cf89901dbf61f6
vikornienko <vikornienko76@gmail.com>
2022-12-17 16:28:05 +0500

    Second commit Settings for static and media. First migration.

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -1,4 +1,4 @@
-"""djangoBlog URL Configuration
+""" djangoBlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
