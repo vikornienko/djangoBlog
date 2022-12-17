4175a4f0cebaed31d9d3ea43d966eb2dfa70f6fd
vikornienko <vikornienko76@gmail.com>
2022-12-17 20:05:40 +0500

    Fifth commit Adding views and templates for posts application.

--- app_posts/views.py
+++ app_posts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Post, PostView, Like, Comment
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    )
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    )
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+
--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -19,9 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from app_posts.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+
 ]
 
 if settings.DEBUG:
