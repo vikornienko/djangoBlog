2f36bb76a84f9e7bfb07d8839b229c467ec0864a
vikornienko <vikornienko76@gmail.com>
2022-12-17 17:11:19 +0500

    Third commit Adding posts application, models for it, pillow library and base template.

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # This project applications
+    'app_posts.apps.AppPostsConfig'
 ]
 
 MIDDLEWARE = [
